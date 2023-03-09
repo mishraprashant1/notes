664fd7fa690a4abced110506745dc0274a2c158e
Prashant <mishraprashant1811998@gmail.com>
2023-03-09 11:24:10 +0530

    Added features to Add a note, update a note, notes index page and pagination

--- apps/notes/urls.py
+++ apps/notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', NotesIndex.as_view(), name='notes_index'),
+    path('', NotesIndexView.as_view(), name='notes_index'),
+    path('create-note/', CreateNoteForm.as_view(), name='create_note'),
 ]
--- apps/notes/views/index.py
+++ apps/notes/views/index.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from apps.notes.models import Notes
 
 
-class NotesIndex(LoginRequiredMixin, TemplateView):
+class NotesIndexView(LoginRequiredMixin, ListView):
     login_url = '/auth'
     template_name = 'notes_index/notes_index.html'
+    model = Notes
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Notes.objects.filter(user=self.request.user).order_by('-modified_at')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'All Notes'
-        print('Something here ----->', self.request)
         context['user_id'] = self.request.user
         return context
